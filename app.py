c27d32d66654b7bfd53862c15910e26f8767cbe5
matej.kaska <nkaskaj@gmail.com>
2022-10-10 22:07:56 +0200

    čtení z csv

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+from asyncio.windows_events import NULL
+from queue import Empty
 from flask import Flask, render_template, request
+import csv
 
-db = {
-    1 : "Poseidon Vltavy",
-    2 : "Tyler Durden"
-}
+db = []
+
+with open("soubory/kek.csv", newline="") as csvfile:
+    csv_reader = csv.reader(csvfile)
+    for row in csv_reader:
+        db.append(row)
+    csvfile.close()
+
 
 flaskAPR = Flask(__name__)
 
@@ -12,8 +19,18 @@ def index():
     if request.method == "GET":
         return render_template("index.html", databaze=db)
     elif request.method == "POST":
+        nazev = request.form["nazev"]
+        nadpis = request.form["nadpis"]
         text = request.form["text"]
-        db[len(db) + 1] = text
+        with open("soubory/kek.csv", mode="r+", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_reader = csv.reader(csvfile)
+            id = 1
+            for row in csv_reader:
+                if row is not "":
+                    id = id + 1
+            csv_writer.writerow([str(id), nazev, nadpis, text])
+            db.append([str(id), nazev, nadpis, text])
         return render_template("index.html", oznameni="Uspesne zaslano", databaze=db)
     return render_template("index.html", databaze=db)
 
