2aeec3934793860bd3e0f2f88671f35bb8d705b3
Boubik <cboubik@gmail.com>
2022-10-15 17:41:51 +0200

    add files and web sites for them

--- app.py
+++ app.py
@@ -1,13 +1,50 @@
 from flask import Flask, render_template, request
 import csv
+from os import listdir
+path = "soubory/"
+endOfFile = "divocak"
+separator = ","
 
 db = []
 
-with open("soubory/kek.csv", newline="") as csvfile:
-    csv_reader = csv.reader(csvfile)
-    for row in csv_reader:
-        db.append(row)
-    csvfile.close()
+def getLineInFile(path):
+    file = open(path, "r")
+    line = file.readline()
+    file.close()
+    return line
+
+def writeLineInFile(path, text):
+    file = open(path, "w")
+    file.write(text)
+    file.close()
+
+def loadDB():
+    for localPath in listdir(path):
+        splitedPath = localPath.split(".")
+        if splitedPath[-1] == endOfFile:
+            meziDB = []
+            meziDB.append(splitedPath[-2].split("/")[-1])
+            for kek in getLineInFile(path + localPath).split(separator):
+                meziDB.append(kek)
+            db.append(meziDB)
+
+def getLastID():
+    listOfFiles = listdir(path)
+    higestNumber = int(listOfFiles[0].split(".")[-2])
+    for value in listOfFiles:
+        fileExtension = value.split(".")[1]
+        if fileExtension == endOfFile:
+            number = int(value.split(".")[0])
+            newNumber = number
+            if newNumber > higestNumber:
+                higestNumber = newNumber
+    return higestNumber
+
+def addToDB(nazev, nadpis, text):
+    id = getLastID() + 1
+    writeLineInFile(path + str(id) + "." + endOfFile, nazev + separator + nadpis + separator + text)
+    db.append([id, nazev, nadpis, text])
+
 
 
 flaskAPR = Flask(__name__)
@@ -20,17 +57,18 @@ def index():
         nazev = request.form["nazev"]
         nadpis = request.form["nadpis"]
         text = request.form["text"]
-        with open("soubory/kek.csv", mode="r+", newline="") as csvfile:
-            csv_writer = csv.writer(csvfile)
-            csv_reader = csv.reader(csvfile)
-            id = 1
-            for row in csv_reader:
-                if row is not "":
-                    id = id + 1
-            csv_writer.writerow([str(id), nazev, nadpis, text])
-            db.append([str(id), nazev, nadpis, text])
+        addToDB(nazev, nadpis, text)
         return render_template("index.html", oznameni="Uspesne zaslano", databaze=db)
     return render_template("index.html", databaze=db)
 
+@flaskAPR.route("/<id>")
+def varName(id):
+    localDB = []
+    for value in db:
+        if value[0] == id:
+            localDB.append(value)
+    return render_template("soubor.html", databaze=db, newDB=localDB)
+
 if __name__ == "__main__":
+    loadDB()
     flaskAPR.run(debug=True)
