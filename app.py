b39f7cb90414cadf008bdd86582423830fcd1e76
matej.kaska <nkaskaj@gmail.com>
2022-11-07 10:41:05 +0100

    Výpisu redis stránky téměř funkční

--- app.py
+++ app.py
@@ -244,23 +244,10 @@ def fakultaRedis(id):
 @flaskAPR.route("/redis")
 def redisTable():
     data = []
-    seznam = {}
     keys = r.scan_iter()
     for key in keys:
         data.append(pickle.loads(r.get(key)))
-    print(len(data))
-    for key in data:
-        key = str(key)
-        sKey = key[key.find("{'") + 2:key.find("':")]
-        key = key[key.find("':") + 4:]
-        sValue = key[:key.find("'")]
-        key = key[key.find("', '") + 4:]
-        sKey2 = key[:key.find("':")]
-        key = key[key.find("':") + 4:]
-        sValue2 = key[:key.find("]]") + 1]
-        seznam.update({sKey: sValue})
-        seznam.update({sKey2: sValue2})
-    return render_template("redis.html", redis=seznam)
+    return render_template("redis.html", redis=data)
 
 if __name__ == "__main__":
     loadDB()
